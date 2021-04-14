589f7dcc45af809b95c44dadce4b5258764d2f0b
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-04-14 08:16:11 -0700

    flipped display of projects list

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,7 @@ from .models import Project
 from django.views.generic import DetailView
 
 def portfolio(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all()[::-1]
     context = {"projects": projects}
     return render(request, 'portfolio/home.html', context)
 
