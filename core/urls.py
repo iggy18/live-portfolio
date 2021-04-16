e98bee25d94552382f42b81e129539a7c7a4480f
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-04-15 18:35:46 -0700

    changed color scheme

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,7 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('', include('portfolio.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^robots\.txt', include('robots.urls')),
